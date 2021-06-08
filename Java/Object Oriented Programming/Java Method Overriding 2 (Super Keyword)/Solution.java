bbfbcdc43870c3a4486d3dc8d14a97762843417c
muth <muthunguclintn@gmail.com>
2021-06-08 11:51:52 +0300

    Object Oriented Programming Commit

---
+++ Java/Object Oriented Programming/Java Method Overriding 2 (Super Keyword)/Solution.java
@@ -0,0 +1,44 @@
+// github.com/Cl9nt-m
+
+import java.util.*;
+
+import java.io.*;
+
+class BiCycle {
+
+    String define_me() {
+
+        return "a vehicle with pedals.";
+
+    }
+
+}
+
+class MotorCycle extends BiCycle {
+
+    String define_me() {
+
+        return "a cycle with an engine.";
+
+    }
+    
+    MotorCycle() {
+
+        System.out.println("Hello I am a motorcycle, I am " + define_me());
+
+        String temp = super.define_me(); // SOLUTION
+
+        System.out.println("My ancestor is a cycle who is " + temp);
+
+    }   
+    
+}
+
+class Solution {
+    public static void main(String[] args) {
+
+        MotorCycle M = new MotorCycle();
+
+    }
+    
+}
